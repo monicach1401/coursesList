6117777c6e9bbb516e4de3739ebb5405a70c8d65
Monica Chavarria <monicachjob@gmail.com>
2023-08-28 16:28:57 +0200

    fetchData

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import './App.css';
+import { QueryClient, QueryClientProvider, useQuery } from "react-query"
+
+const Banner = ({ title }) => (
+  <h1>{ title }</h1>
+);
+
+const CourseList = ({ courses }) => {
+  const [term, setTerm] = useState('Fall');
+  const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
+  
+  return (
+    <>
+      <TermSelector term={term} setTerm={setTerm} />
+      <div className="course-list">
+      { termCourses.map(course => <Course key={course.id} course={ course } />) }
+      </div>
+   </>
+  );
+};
+const TermButton = ({term, setTerm, checked}) => (
+  <>
+    <input type="radio" id={term} className="btn-check" checked={checked} autoComplete="off"
+      onChange={() => setTerm(term)} />
+    <label class="btn btn-success m-1 p-2" htmlFor={term}>
+    { term }
+    </label>
+  </>
+);
+
+const TermSelector = ({term, setTerm}) => (
+  <div className="btn-group">
+  { 
+    Object.values(terms).map(value => (
+      <TermButton key={value} term={value} setTerm={setTerm} checked={value === term} />
+    ))
+  }
+  </div>
+);
+const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
+
+const getCourseTerm = course => (
+  terms[course.id.charAt(0)]
+);
+
+const getCourseNumber = course => (
+  course.id.slice(1, 4)
+);
+
+const Course = ({ course }) => (
+  <div className="card m-1 p-2">
+    <div className="card-body">
+      <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
+      <div className="card-text">{ course.title }</div>
+    </div>
+  </div>
+);
+
+const fetchSchedule = async () => {
+  const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
+  const response = await fetch(url);
+  if (!response.ok) throw response;
+  return await response.json();
+};
+
+const Main = () =>  {
+  const { data:schedule, isLoading, error } = useQuery('schedule', fetchSchedule);
+  
+  if (error) return <h1>{error}</h1>;
+  if (isLoading) return <h1>Loading the schedule...</h1>
+
+  return (
+    <div className="container">
+      <Banner title={ schedule.title } />
+      <CourseList courses={ schedule.courses } />
+    </div>
+  );
+};
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <Main />
+  </QueryClientProvider>
+);
+export default App;
