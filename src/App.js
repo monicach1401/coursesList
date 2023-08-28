a35c85135de74c6a6c31a05cc17d7bb8d8d8edcb
Monica Chavarria <monicachjob@gmail.com>
2023-08-28 16:33:57 +0200

    selectAndUnselectCourses

--- src/App.js
+++ src/App.js
@@ -8,15 +8,21 @@ const Banner = ({ title }) => (
 
 const CourseList = ({ courses }) => {
   const [term, setTerm] = useState('Fall');
+  const [selected, setSelected] = useState([]);
   const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
   
   return (
     <>
       <TermSelector term={term} setTerm={setTerm} />
       <div className="course-list">
-      { termCourses.map(course => <Course key={course.id} course={ course } />) }
+      { 
+        termCourses.map(course =>
+          <Course key={ course.id } course={ course }
+            selected={selected} setSelected={ setSelected } 
+          />) 
+      }
       </div>
-   </>
+    </>
   );
 };
 const TermButton = ({term, setTerm, checked}) => (
@@ -47,16 +53,25 @@ const getCourseTerm = course => (
 const getCourseNumber = course => (
   course.id.slice(1, 4)
 );
-
-const Course = ({ course }) => (
-  <div className="card m-1 p-2">
-    <div className="card-body">
-      <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
-      <div className="card-text">{ course.title }</div>
-    </div>
-  </div>
+const toggle = (x, lst) => (
+  lst.includes(x) ? lst.filter(y => y !== x) : [x, ...lst]
 );
-
+const Course = ({ course, selected, setSelected }) => {
+  const isSelected = selected.includes(course);
+  const style = {
+    backgroundColor: isSelected ? 'lightgreen' : 'white'
+  };
+  return (
+    <div className="card m-1 p-2" 
+      style={style}
+      onClick={() => setSelected(toggle(course, selected))}>
+      <div className="card-body">
+        <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
+        <div className="card-text">{ course.title }</div>
+      </div>
+    </div>
+  );
+};
 const fetchSchedule = async () => {
   const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
   const response = await fetch(url);
