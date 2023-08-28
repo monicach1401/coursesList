fd2deebe489a7e2f03c27092146a61657262c68d
Monica Chavarria <monicachjob@gmail.com>
2023-08-28 16:37:11 +0200

    filterByTimeConflicts

--- src/App.js
+++ src/App.js
@@ -58,27 +58,74 @@ const toggle = (x, lst) => (
 );
 const Course = ({ course, selected, setSelected }) => {
   const isSelected = selected.includes(course);
+  const isDisabled = !isSelected && hasConflict(course, selected);
   const style = {
-    backgroundColor: isSelected ? 'lightgreen' : 'white'
+    backgroundColor: isDisabled? 'lightgrey' : isSelected ? 'lightgreen' : 'white'
   };
   return (
     <div className="card m-1 p-2" 
       style={style}
-      onClick={() => setSelected(toggle(course, selected))}>
+      onClick={isDisabled ? null : () =>  setSelected(toggle(course, selected))}>
       <div className="card-body">
         <div className="card-title">{ getCourseTerm(course) } CS { getCourseNumber(course) }</div>
         <div className="card-text">{ course.title }</div>
+        <div className="card-text">{ course.meets }</div>
       </div>
     </div>
   );
 };
+const hasConflict = (course, selected) => (
+  selected.some(selection => courseConflict(course, selection))
+);
+const meetsPat = /^ *((?:M|Tu|W|Th|F)+) +(\d\d?):(\d\d) *[ -] *(\d\d?):(\d\d) *$/;
+
+const timeParts = meets => {
+  const [match, days, hh1, mm1, hh2, mm2] = meetsPat.exec(meets) || [];
+  return !match ? {} : {
+    days,
+    hours: {
+      start: hh1 * 60 + mm1 * 1,
+      end: hh2 * 60 + mm2 * 1
+    }
+  };
+};
+const mapValues = (fn, obj) => (
+  Object.fromEntries(Object.entries(obj).map(([key, value]) => [key, fn(value)]))
+);
+
+const addCourseTimes = course => ({
+  ...course,
+  ...timeParts(course.meets)
+});
+
+const addScheduleTimes = schedule => ({
+  title: schedule.title,
+  courses: mapValues(addCourseTimes, schedule.courses)
+});
 const fetchSchedule = async () => {
   const url = 'https://courses.cs.northwestern.edu/394/data/cs-courses.php';
   const response = await fetch(url);
   if (!response.ok) throw response;
-  return await response.json();
+  return addScheduleTimes(await response.json());
 };
+const days = ['M', 'Tu', 'W', 'Th', 'F'];
 
+const daysOverlap = (days1, days2) => ( 
+  days.some(day => days1.includes(day) && days2.includes(day))
+);
+
+const hoursOverlap = (hours1, hours2) => (
+  Math.max(hours1.start, hours2.start) < Math.min(hours1.end, hours2.end)
+);
+
+const timeConflict = (course1, course2) => (
+  daysOverlap(course1.days, course2.days) && hoursOverlap(course1.hours, course2.hours)
+);
+
+const courseConflict = (course1, course2) => (
+  getCourseTerm(course1) === getCourseTerm(course2)
+  && timeConflict(course1, course2)
+);
 const Main = () =>  {
   const { data:schedule, isLoading, error } = useQuery('schedule', fetchSchedule);
   
